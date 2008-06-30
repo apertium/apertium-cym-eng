6a553fe245dfc7fb1642e861722ca0fb0a25cac2
Francis M. Tyers <ftyers@prompsit.com>
2008-06-30 11:11:33 +0000

    Add regression script

---
+++ regression-tests.sh
@@ -0,0 +1,21 @@
+LIST=`wget -O - -q http://wiki.apertium.org/wiki/Welsh_to_English/Regression_tests | grep '<li>' | sed 's/<.*li>//g' | sed 's/ /_/g'`;
+
+cp *.mode modes/
+
+for LINE in $LIST; do
+	dir=`echo $LINE | cut -f2 -d'(' | cut -f1 -d')'`;
+
+	if [ $dir = "en" ]; then
+		mode="en-cy";
+	elif [ $dir = "cy" ]; then
+		mode="cy-en";
+	else 
+		continue;
+	fi
+
+#	echo $LINE;
+	SL=`echo $LINE | cut -f2 -d')' | sed 's/<i>//g' | sed 's/<\/i>//g' | cut -f2 -d'*' | sed 's/→/@/g' | cut -f1 -d'@' | sed 's/(note:/@/g' | sed 's/_/ /g'`;
+
+	echo -e $mode"\t"$SL"\t"`echo $SL | apertium -d . $mode`;
+
+done
