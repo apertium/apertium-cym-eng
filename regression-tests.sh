841c06e459905f2bf6c37a323380646132ba1e37
Francis M. Tyers <ftyers@prompsit.com>
2008-07-27 11:16:02 +0000

    Fix regression test script

--- regression-tests.sh
+++ regression-tests.sh
@@ -17,6 +17,10 @@ for LINE in $LIST; do
 	SL=`echo $LINE | cut -f2 -d')' | sed 's/<i>//g' | sed 's/<\/i>//g' | cut -f2 -d'*' | sed 's/→/@/g' | cut -f1 -d'@' | sed 's/(note:/@/g' | sed 's/_/ /g'`;
 	TL=`echo $LINE | sed 's/(\w\w)//g' | sed 's/<i>//g' | cut -f2 -d'*' | sed 's/<\/i>_→/@/g' | cut -f2 -d'@' | sed 's/_/ /g'`;
 
-	echo -e $mode"\t "$SL"\n\t-$TL\n\t+ "`echo $SL | apertium -d . $mode`"\n";
+	TR=`echo $SL | apertium -d . $mode`;
+
+	if [[ `echo $TR | tr '[A-Z]' '[a-z]'` != `echo $TL | tr '[A-Z]' '[a-z]'` ]]; then 
+		echo -e $mode"\t "$SL"\n\t-$TL\n\t+ "$TR"\n";
+	fi
 
 done
